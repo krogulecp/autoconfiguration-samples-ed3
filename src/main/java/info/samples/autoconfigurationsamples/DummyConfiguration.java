0a717b7a398bac10b01f2f46892ea7608eb3fee0
Piotr Krogulec <piotr.krogulec@allegro.pl>
2021-01-16 12:44:54 +0100

    t3i4

--- src/main/java/info/samples/autoconfigurationsamples/DummyConfiguration.java
+++ src/main/java/info/samples/autoconfigurationsamples/DummyConfiguration.java
@@ -1,12 +1,16 @@
 package info.samples.autoconfigurationsamples;
 
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnWebApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@ConditionalOnWebApplication(type = ConditionalOnWebApplication.Type.SERVLET)
 public class DummyConfiguration {
 
     @Bean
+    @ConditionalOnMissingBean
     public String dummyString(){
         return "dummyString";
     }
